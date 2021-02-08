2ceaae78840e8ba17f9eff7c09b3a12c496b4be2
Alexis Roger <a.roger37@gmail.com>
2021-02-08 13:07:48 +0100

    changé sqlite pour postgre

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_02_08_074355) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "argonauts", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Seeding DB:'
+print '\n Création reussie: "."'
+print ' - '
+print ' Création echouée: "X"'
+puts '\n\n Création de 5 Argonautes'
+
+atalante = Argonaut.new(name: 'Atalante')
+if atalante.save
+  print('.')
+else
+  print('X')
+end
+
+erginos = Argonaut.new(name: 'Erginos')
+if erginos.save
+  print('.')
+else
+  print('X')
+end
+
+thesee = Argonaut.new(name: 'Thésée')
+if thesee.save
+  print('.')
+else
+  print('X')
+end
+
+nauplios = Argonaut.new(name: 'Nauplios fils de Clytonée')
+if nauplios.save
+  print('.')
+else
+  print('X')
+end
+
+jason = Argonaut.new(name: 'Jason')
+if jason.save
+  print('.')
+else
+  print('X')
+end
